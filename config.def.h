36e88417f7efc17bb1270ee7df46ba0517f8bf99
Metin Gemil <metin.gemil@keysight.com>
2021-06-11 14:22:35 +0300

    ubuntu conf file

--- config.def.h
+++ config.def.h
@@ -1,5 +1,5 @@
 /* macro for conveniently specifying pathu and pathc below */
-#define PATH(name)                      "/home/limegch/.local/dwmblocks/blocks/"name
+#define PATH(name)                      "/home/metgemil/.local/dwmblocks/blocks/"name
 
 /* buffer size for capturing output of the programs used for updating blocks */
 #define CMDOUTLENGTH                    50
